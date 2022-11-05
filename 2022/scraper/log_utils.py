843ccc32dc55109cdbfc1ac629a620a18c4c8fcd
Abhishek Jain <abhishek.raj@zecdata.com>
2022-11-05 16:06:32 +0530

    log file

--- 2022/scraper/log_utils.py
+++ 2022/scraper/log_utils.py
@@ -1,12 +1,13 @@
 import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler("logs/debug.log"),
-        # logging.StreamHandler()
-    ]
-)
-
-rootLogger = logging.getLogger()
+def create_log_object(county_name, log_file_name_refex=''):
+    log_file_name = f"logs/debug_{county_name}.log"
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(log_file_name),
+            # logging.StreamHandler()
+        ]
+    )
+    rootLogger = logging.getLogger()
+    return rootLogger
